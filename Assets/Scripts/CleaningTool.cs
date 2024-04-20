4dbd088562b2bb41d666e4a7a5b043594a2dca35
Crashim03 <alexteloduarte@gmail.com>
2024-04-20 14:06:32 +0200

    Brush is working

--- Assets/Scripts/CleaningTool.cs
+++ Assets/Scripts/CleaningTool.cs
@@ -5,21 +5,55 @@ using System.Collections.Generic;
 public class CleaningTool : MonoBehaviour
 {
     public GameObject Brush;
+    [SerializeField] private GameObject _mirror;
     [SerializeField] private GameObject _cleanLayer;
-    private readonly HashSet<Vector2> positions = new();
+    [SerializeField] private float _interval = 0.5f;
+    private readonly HashSet<Vector2> _brushPositions = new();
+    private readonly List<Vector2> _toClean = new();
+    private Bounds _bounds;
+    private float _brushRadius;
+    private float _toCleanCount;
 
     public void Move(Vector3 position)
     {
-        Vector2 pos2D = new (position.x, position.y);
+       if (position.x > _bounds.max.x || position.x < _bounds.min.x || 
+        position.y > _bounds.max.y || position.y < _bounds.min.y)
+        {
+            return;
+        }
+        Vector2 coordinates = new (position.x, position.y);
 
-        if (!positions.Contains(pos2D))
+        if (!_brushPositions.Contains(coordinates))
         {
             Instantiate(Brush, position, quaternion.identity, _cleanLayer.transform);
-            positions.Add(pos2D);
+            _brushPositions.Add(coordinates);
         }
-        else
+        foreach (Vector2 coordinate in new List<Vector2>(_toClean))
         {
-            Debug.Log("Already there!");
+            if (Vector2.Distance(coordinate, position) <= _brushRadius)
+            {
+                _toClean.Remove(coordinate);
+            }
         }
+        Debug.Log(_toClean.Count / _toCleanCount * 100);
+    }
+
+    private void Start()
+    {
+        for (float i = _bounds.min.x; i < _bounds.max.x; i += _interval)
+        {
+            for (float j = _bounds.min.y; j < _bounds.max.y; j += _interval)
+            {
+                _toClean.Add(new Vector2(i, j));
+            }
+        }
+        _toCleanCount = _toClean.Count;
+        Debug.Log(_toCleanCount);
+    }
+
+    private void Awake()
+    {
+        _bounds = _mirror.GetComponent<Collider2D>().bounds;
+        _brushRadius = Brush.GetComponent<SpriteMask>().bounds.size.x / 2;
     }
 }
